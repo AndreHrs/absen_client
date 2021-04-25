25bf819fe04e07eb311f6ef331cfbcccdf630a3d
unknown <andreharsono27@gmail.com>
2021-04-25 12:28:29 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="card">
+      <div class="card-title">
+        <h1 class="title">DAFTAR NAMA PENGAMBILAN JAKET</h1>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div v-for="data in dataList" :key="data.nim" class="col-12">
+            <CardComponent :data="data" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CardComponent from "./components/CardComponent.vue";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:5050";
+var socket = null;
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      message: "",
+      dataList: [],
+    };
+  },
+  methods: {
+    pushData(data) {
+      this.dataList.unshift(data.value);
+      console.log(this.dataList);
+      if (this.dataList.length >= 5) {
+        console.log("list bigger than 10");
+        this.dataList.pop();
+        console.log(this.dataList);
+      }
+    },
+  },
+  components: { CardComponent },
+  created() {
+    let self = this;
+    socket = socketIOClient(ENDPOINT);
+    socket.on("request_data", (data) => {
+      self.message = data.value.nama;
+      console.log(data.value);
+      self.pushData(data);
+    });
   },
 };
 </script>
@@ -21,6 +60,26 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.title {
+  padding-top: 20px;
+}
+.bold {
+  font-weight: bold;
+}
+.container {
+  padding: 20px;
+  /* The image used */
+}
+body {
+  background-image: url("./assets/bg.jpg");
+
+  /* Full height */
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
 }
 </style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card card-body card-container">
+    <div class="row">
+      <div class="col-sm-4 col-12"><h2>NIM</h2></div>
+      <div class="col-sm-8 col-12">
+        <h2 class="bold">{{ splicedNim }}</h2>
+      </div>
+
+      <div class="col-sm-4 col-12"><h2>NAMA</h2></div>
+      <div class="col-sm-8 col-12">
+        <h2 class="bold">{{ data.nama }}</h2>
+      </div>
+
+      <div class="col-sm-4 col-12"><h2>UKURAN JAKET</h2></div>
+      <div class="col-sm-8 col-12">
+        <h2 class="bold">{{ data.jaket }}</h2>
+      </div>
+      <div class="col-sm-4 col-12"><h2>KELAS</h2></div>
+      <div class="col-sm-8 col-12">
+        <h2 class="bold">{{ data.kelas }}</h2>
+      </div>
+      <div class="col-sm-4 col-12"><h2>HARI PENGAMBILAN</h2></div>
+      <div class="col-sm-8 col-12">
+        <h2 class="bold">{{ data.hari }}</h2>
+      </div>
+      <div class="col-sm-4 col-12"><h2>KETERANGAN</h2></div>
+      <div class="col-sm-8 col-12">
+        <h2 class="bold">{{ data.keterangan }}</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { data: Object },
+  name: "CardComponent",
+  computed: {
+    splicedNim() {
+      let temp = String(this.data.nim);
+      return (
+        temp.substring(0, 2) +
+        " . " +
+        temp.substring(2, 5) +
+        " . " +
+        temp.substring(5, 10)
+      );
+    },
+  },
+};
+</script>
+
+<style>
+.card-container {
+  width: 100%;
+  padding: 30px;
+  border-radius: 20px;
+  margin-bottom: 20px;
+  text-align: left;
+}
+</style>
